a607efe46b19a10f6b4b0b96f9daadabfc866c24
PajTajster <38697671+PajTajster@users.noreply.github.com>
2019-03-06 14:07:17 +0100

    Added sample usage of rail fence

--- BSK01/BSK01/Program.cs
+++ BSK01/BSK01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BSK01.Ciphers;
 
 namespace BSK01
 {
@@ -6,7 +7,38 @@ namespace BSK01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n;
+            Console.WriteLine("Give n:");
+            try
+            {
+                n = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid input. rip");
+                Console.ReadKey();
+
+                return;
+            }
+            Console.WriteLine("Give string:");
+            string textToEncode;
+            textToEncode = Console.ReadLine();
+
+
+            RailFenceCipher rfc = new RailFenceCipher(n);
+
+            string encryptedText = rfc.Encode(textToEncode);
+
+            Console.WriteLine("enkrypszon: " + encryptedText);
+
+
+            string decodedText = rfc.Decode(encryptedText);
+
+            Console.WriteLine("dekrypszon: " + decodedText);
+
+            Console.ReadKey();
         }
+
+
     }
 }
