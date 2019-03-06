974df6326d45f6ec27c79de6cf719b84e51bbb40
PajTajster <38697671+PajTajster@users.noreply.github.com>
2019-03-06 14:06:49 +0100

    Base for Vigener Cipher class

---
+++ BSK01/BSK01/Ciphers/VigenerCipher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSK01.Ciphers
+{
+    class VigenerCipher
+    {
+        // encode: (K + W) mod n
+        // encode: (C - R) mod n
+
+        string key;
+
+        public VigenerCipher(string k)
+        {
+            key = k;
+        }
+
+        string Encode(string text)
+        {
+            return "";
+        }
+        string Decode(string text)
+        {
+            return "";
+        }
+    }
+}
