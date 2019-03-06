45dee1425c5c014e202a8e026ecbb3265294724e
PajTajster <38697671+PajTajster@users.noreply.github.com>
2019-03-06 14:05:39 +0100

    Added base for Transposition Cipher 2 class

---
+++ BSK01/BSK01/Ciphers/TranspositionCipher2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSK01.Ciphers
+{
+    class TranspositionCipher2
+    {
+        string key;
+
+        public TranspositionCipher2(string k)
+        {
+            key = k;
+        }
+
+        string Encode(string text)
+        {
+            return "";
+        }
+        string Decode(string text)
+        {
+            return "";
+        }
+    }
+}
