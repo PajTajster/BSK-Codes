0df73841ba73c7ee52409289d94bcd3c0676350b
PajTajster <38697671+PajTajster@users.noreply.github.com>
2019-03-22 18:45:31 +0100

    Implemented LSFRGenerator

--- BSK02/BSK02/Generators/LSFRGenerator.cs
+++ BSK02/BSK02/Generators/LSFRGenerator.cs
@@ -6,5 +6,80 @@ namespace BSK02.Generators
 {
     class LSFRGenerator
     {
+        private bool[] polynomial;
+        private bool[] seed;
+        private int outAmount;
+
+        public LSFRGenerator(string polynomial, string seed, int outAmount)
+        {
+            this.polynomial = new bool[polynomial.Length];
+            this.seed = new bool[seed.Length];
+
+            for(int i = 0; i < polynomial.Length; ++i)
+            {
+                this.polynomial[i] = polynomial[i] == '1';
+                this.seed[i] = seed[i] == '1';
+            }
+            this.outAmount = outAmount;
+        }
+
+        public string[] Generate()
+        {
+            int rowLength = polynomial.Length;
+
+            string[] generatedBytes = new string[outAmount];
+
+            bool[,] bytesArray = new bool[outAmount + 1, rowLength];
+
+            for (int i = 0; i < rowLength; ++i) 
+            {
+                bytesArray[0, i] = seed[i];
+            }
+
+            List<bool> bytesToXOR = new List<bool>();
+
+            for (int i = 0; i < outAmount + 1; ++i)
+            {
+                for (int j = 0; j < rowLength; ++j)
+                {
+                    if (polynomial[j]) 
+                    {
+                        bytesToXOR.Add(bytesArray[i, j]);
+                    }
+                }               
+
+                // XOR
+                bool resultXOR = bytesToXOR[0];
+                for (int k = 1; k < bytesToXOR.Count; ++k)
+                    resultXOR ^= bytesToXOR[k];
+
+                if(i < outAmount)
+                {
+                    // shift
+                    for (int j = 0; j < rowLength - 1; ++j)
+                    {
+                        bytesArray[i + 1, j + 1] = bytesArray[i, j];
+                    }
+                    bytesArray[i + 1, 0] = resultXOR;
+
+
+                    for (int j = 0; j < rowLength; ++j)
+                    {
+                        if (bytesArray[i + 1, j])
+                        {
+                            generatedBytes[i] += "1";
+                        }
+                        else
+                        {
+                            generatedBytes[i] += "0";
+                        }
+                    }
+                }
+
+                bytesToXOR.Clear();
+            }
+
+            return generatedBytes;
+        }
     }
 }
--- BSK02/BSK02/Program.cs
+++ BSK02/BSK02/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
+using BSK02.Generators;
+
 namespace BSK02
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string outputPoly = "";
+            string outputSeed = "";
+            string n = "";
+
+
+            Console.WriteLine("Give polynomial: ");
+            outputPoly = Console.ReadLine();
+
+            Console.WriteLine("Give seed: ");
+            outputSeed = Console.ReadLine();
+            
+            Console.WriteLine("Give n: ");
+            n = Console.ReadLine();
+
+            LSFRGenerator generator = new LSFRGenerator(outputPoly, outputSeed, int.Parse(n));
+
+            string[] result = generator.Generate();
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
