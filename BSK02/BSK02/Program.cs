4d000d727aa00f52be60cadb2ced539c3487c00f
PajTajster <38697671+PajTajster@users.noreply.github.com>
2019-03-24 21:31:57 +0100

    Added simple menu.

--- BSK02/BSK02/Program.cs
+++ BSK02/BSK02/Program.cs
@@ -9,10 +9,100 @@ namespace BSK02
     {
         static void Main(string[] args)
         {
-            string outputPoly = "";
-            string outputSeed = "";
-            string n = "";
+            string[] userInputStrings = new string[3];
 
+            while (true)
+            {
+                string option = "_";
+
+                Console.WriteLine("1. LSFRGenerator\n" +
+                    "2. Synchronous Steam Cipher\n" +
+                    "3. Ciphertext Autokey\n" +
+                    "4. Exit");
+                Console.Write("Option: ");
+                option = Console.ReadLine();
+
+
+                switch(option[0])
+                {
+                    case '1':
+                        {
+                            userInputStrings = AskForUserInput(false);
+                            LSFRGenerator lsfr = new LSFRGenerator(userInputStrings[0],
+                                                                    userInputStrings[1],
+                                                                    int.Parse(userInputStrings[2]));
+
+                            string[] output = lsfr.Generate();
+
+                            Console.WriteLine("Generated codes: ");
+                            foreach (var item in output)
+                            {
+                                Console.WriteLine(item);
+                            }
+
+                            Console.WriteLine("\nPress any key to continue.");
+                            Console.ReadKey();
+                        }
+                        break;
+                    case '2':
+                        {
+                            userInputStrings = AskForUserInput(true);
+                            SynchronousStreamCipher ssc = new SynchronousStreamCipher(userInputStrings[0],
+                                                                                    userInputStrings[1],
+                                                                                    userInputStrings[2]);
+
+                            string output = ssc.Encipher();
+
+                            Console.WriteLine("Enciphered output: " + output);
+
+                            Console.WriteLine("\nDeciphering:");
+                            userInputStrings = AskForUserInput(true);
+                            ssc = new SynchronousStreamCipher(userInputStrings[0],
+                                                            userInputStrings[1],
+                                                            userInputStrings[2]);
+
+                            output = ssc.Encipher();
+
+                            Console.WriteLine("Deciphered output: " + output);
+
+                            Console.WriteLine("\nPress any key to continue.");
+                            Console.ReadKey();
+                        }
+                        break;
+                    case '3':
+                        {
+                            userInputStrings = AskForUserInput(true);
+                            CiphertextAutokey cta = new CiphertextAutokey(userInputStrings[0],
+                                                                                    userInputStrings[1],
+                                                                                    userInputStrings[2]);
+
+                            string output = cta.Encipher();
+
+                            Console.WriteLine("Enciphered output: " + output);
+
+                            Console.WriteLine("\nDeciphering:");
+                            userInputStrings = AskForUserInput(true);
+                            cta = new CiphertextAutokey(userInputStrings[0],
+                                                            userInputStrings[1],
+                                                            userInputStrings[2]);
+
+                            output = cta.Decipher();
+
+                            Console.WriteLine("Deciphered output: " + output);
+
+                            Console.WriteLine("\nPress any key to continue.");
+                            Console.ReadKey();
+                        }
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        break;
+                }
+            }
+
+        
+            /*
 
             Console.WriteLine("Give polynomial: ");
             outputPoly = Console.ReadLine();
@@ -25,7 +115,7 @@ namespace BSK02
 
             CiphertextAutokey ca = new CiphertextAutokey(outputPoly, outputSeed, n);
 
-            string result = ca.Decipher();
+            string result = ca.Encipher();
 
            // SynchronousStreamCipher ssc = new SynchronousStreamCipher(outputPoly, outputSeed, n);
            // string result = ssc.Encipher();
@@ -37,7 +127,43 @@ namespace BSK02
             foreach (var item in result)
             {
                 Console.Write(item);
+            */
+        }
+
+        private static string[] AskForUserInput(bool isCipher)
+        {
+            string[] output = new string[3];
+            bool isCorrectInput = false;
+
+            while (!isCorrectInput)
+            {
+                Console.WriteLine("Specify polynominal: ");
+                output[0] = Console.ReadLine();
+
+                Console.WriteLine("Specify seed:");
+                output[1] = Console.ReadLine();
+
+                if(output[0].Length != output[1].Length)
+                {
+                    Console.WriteLine("Seed and polynomial should be same length!\n\n");
+                }
+                else
+                {
+                    isCorrectInput = true;
+                }
             }
+
+            if (!isCipher)
+            {
+                Console.WriteLine("Give n: ");
+                output[2] = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Give X [if file type name/path]:");
+                output[2] = Console.ReadLine();
+            }
+            return output;
         }
     }
 }
