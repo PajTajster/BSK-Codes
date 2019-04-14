a4b5ca04a36e3567564e05e4b7253101732b44c6
PajTajster <38697671+PajTajster@users.noreply.github.com>
2019-04-14 18:23:06 +0200

    Implemented Decrypt

--- BSK03/BSK03/DES/DES.cs
+++ BSK03/BSK03/DES/DES.cs
@@ -80,7 +80,73 @@ namespace BSK03.DES
         }
         public bool[] Decrypt(bool[] data, bool[] key)
         {
-            throw new NotImplementedException();
+            KeyGenerator keys = new KeyGenerator(key);
+
+            int extraChunks = 0;
+            if(data.Length % 64 != 0)
+            {
+                extraChunks = data.Length % 64;
+            }
+            bool[] tmp = data;
+            Array.Resize(ref data, data.Length + extraChunks);
+            tmp.CopyTo(data, 0);
+
+            var chunks = SplitData(data, 64);
+
+            List<bool[]> connectedBlocks = new List<bool[]>();
+
+            for (int i = 0; i < chunks.Count; ++i)
+            {
+                var block = chunks[i];
+                block = InitialPermute(block);
+
+                bool[] leftBuffer, rightBuffer;
+
+                bool[] leftSplit = new bool[32];
+                bool[] rightSplit = new bool[32];
+
+                Array.Copy(block, 0, leftSplit, 0, 32);
+                Array.Copy(block, 32, rightSplit, 0, 32);
+                leftBuffer = leftSplit;
+                rightBuffer = rightSplit;
+
+                for (int j = 15; j > 0; --j)
+                {
+                    leftSplit = rightSplit;
+                    rightBuffer = FeistelFunction(rightSplit, keys.generatedKeys, i);
+
+                    for (int k = 0; k < 32; ++k)
+                    {
+                        rightSplit[k] = leftBuffer[k] ^ rightBuffer[k];
+                    }
+                }
+
+                leftBuffer = leftSplit;
+                rightBuffer = FeistelFunction(rightSplit, keys.generatedKeys, 0);
+                for (int j = 0; j < 32; ++j)
+                {
+                    leftSplit[j] = leftBuffer[j] ^ rightBuffer[j];
+                }
+
+                Array.Copy(leftSplit, 0, block, 0, 32);
+                Array.Copy(rightSplit, 0, block, 32, 32);
+
+                block = FinalPermute(block);
+
+                connectedBlocks.Add(block);
+            }
+
+            int outputSize = connectedBlocks.Count * 64;
+            outputSize -= extraChunks;
+
+            bool[] output = new bool[outputSize];
+            for (int i = 0; i < connectedBlocks.Count - 1; ++i)
+            {
+                Array.Copy(connectedBlocks[i], 0, output, i * 64, 64);
+            }
+            Array.Copy(connectedBlocks[connectedBlocks.Count - 1], 0, output, outputSize - 64, 64 - extraChunks);
+
+            return output;
         }
 
         private bool[] FeistelFunction(bool[] dataRightPart, bool[] key, int keyNumber)
