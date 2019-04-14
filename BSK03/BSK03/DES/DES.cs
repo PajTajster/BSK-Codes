95f8c13738a2b740cf23a91f62086956233441e4
PajTajster <38697671+PajTajster@users.noreply.github.com>
2019-04-14 10:48:17 +0200

    Created main alghorithm and generator classes.

---
+++ BSK03/BSK03/DES/DES.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSK03.DES
+{
+    class DES
+    {
+
+    }
+}
---
+++ BSK03/BSK03/DES/KeyGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSK03.DES
+{
+    class KeyGenerator
+    {
+        private byte[] cKeyPart = new byte[28];
+        private byte[] dKeyPart = new byte[28];
+
+        public byte[,] generatedKeys = new byte[16, 48];
+
+
+        public void GenerateKeys(byte[] key)
+        {
+            for (int i = 0; i < 28; ++i) ;
+            {
+
+            }
+        } 
+
+        private byte[] Shift(byte[] keyPart)
+        {
+            byte buffer = keyPart[0];
+            for (int i = 0; i < 27; ++i)
+            {
+                keyPart[i] = keyPart[i + 1];
+            }
+
+            keyPart[27] = buffer;
+
+            return keyPart;
+        }
+    }
+}
