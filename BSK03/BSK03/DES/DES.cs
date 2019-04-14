6d80132cda19066321918f637c385421240e7e3a
PajTajster <38697671+PajTajster@users.noreply.github.com>
2019-04-14 11:55:41 +0200

    Added initial and final permutations and defined crucial methods. [implement later]

--- BSK03/BSK03/DES/DES.cs
+++ BSK03/BSK03/DES/DES.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,40 @@ namespace BSK03.DES
 {
     class DES
     {
+        public BitArray Encrypt(BitArray data, BitArray key)
+        {
+            throw new NotImplementedException();
+        }
+        public BitArray Decrypt(BitArray data, BitArray key)
+        {
+            throw new NotImplementedException();
+        }
 
+        private BitArray FeistelFunction(BitArray dataRightPart, BitArray key)
+        {
+            throw new NotImplementedException();
+        }
+        private BitArray InitialPermute(BitArray data)
+        {
+            BitArray result = new BitArray(64);
+
+            for (int i = 0; i < 64; ++i)
+            {
+                result[i] = data[Permutations.initialPermutation[i] - 1];
+            }
+
+            return result;
+        }
+        private BitArray FinalPermute(BitArray data)
+        {
+            BitArray result = new BitArray(64);
+
+            for (int i = 0; i < 64; ++i)
+            {
+                result[i] = data[Permutations.finalPermutation[i] - 1];
+            }
+
+            return result;
+        }
     }
 }
